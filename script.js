820cd409623ae4b0140a8d4c713220b95c3bb10a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-19 20:37:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function setColorRed() {
+    document.getElementsByClassName("yellow")[0].style.background = "grey";
+    document.getElementsByClassName("red")[0].style.background = "red";
+    document.getElementsByClassName("green")[0].style.background = "grey";
+}
+
+
+
+function setColorYellow() {
+    document.getElementsByClassName("yellow")[0].style.background = "yellow";
+    document.getElementsByClassName("red")[0].style.background = "grey";
+    document.getElementsByClassName("green")[0].style.background = "grey";
+}
+
+function setColorGreen() {
+    document.getElementsByClassName("yellow")[0].style.background = "grey";
+    document.getElementsByClassName("red")[0].style.background = "grey";
+    document.getElementsByClassName("green")[0].style.background = "green";
+}
